2daecf83f4b216d97ce888a335ac77fec2237440
ReddyAnanya9 <58568034+ReddyAnanya9@users.noreply.github.com>
2020-04-10 04:03:49 +0530

    Made changes to the signup file and also added a database. Next is to add google signin and authenticate with firebase. If possible try to do email validation too

--- app/src/main/java/com/example/sharecab/signup.java
+++ app/src/main/java/com/example/sharecab/signup.java
@@ -3,6 +3,7 @@ package com.example.sharecab;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.multidex.MultiDexApplication;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,30 +17,42 @@ import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 import static android.content.ContentValues.TAG;
 
-public class signup extends AppCompatActivity {
-    EditText mFullName,mEmail,mPassword;
+public class signup extends AppCompatActivity{
+    EditText mFullName,mEmail,mPassword,checkPass,mphoneno;
     Button mRegisterBtn;
     TextView mLoginBtn;
+    String userID;
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
         mFullName = findViewById(R.id.editText4);
         mEmail = findViewById(R.id.editText6);
         mPassword = findViewById(R.id.editText7);
         mRegisterBtn = findViewById(R.id.registerbtn);
         mLoginBtn = findViewById(R.id.textView6);
+        checkPass=findViewById(R.id.editText8);
+        mphoneno=findViewById(R.id.editText5);
         mAuth = FirebaseAuth.getInstance();
+        db=FirebaseFirestore.getInstance();
         if(mAuth.getCurrentUser()!= null){
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
@@ -47,9 +60,11 @@ public class signup extends AppCompatActivity {
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmail.getText().toString().trim();
+                final String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
-
+                final String fullname = mFullName.getText().toString();
+                final String phoneno = mphoneno.getText().toString();
+                String checkpass = checkPass.getText().toString();
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("email required");
                     return;
@@ -58,19 +73,43 @@ public class signup extends AppCompatActivity {
                     mPassword.setError("Password required");
                     return;
                 }
-
+                if(phoneno.length()!=10){
+                    mphoneno.setError("Invalid phone no");
+                }
                 if(password.length()<6){
                     mPassword.setError("Password grater than 6");
                     return;
                 }
-                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(signup.this,new OnCompleteListener<AuthResult>() {
+                if(!password.equals(checkpass)){
+                    checkPass.setError("password doesn't match");
+                }
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(signup.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(signup.this,"User created",Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }else{
-                            Toast.makeText(signup.this,"Error occurred" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(signup.this, "User created", Toast.LENGTH_SHORT).show();
+                            userID = mAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = db.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("fname", fullname);
+                            user.put("email", email);
+                            user.put("phone no.", phoneno);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "user profile created for " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "failure occurred" + e.toString());
+                                }
+                            });
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(signup.this, "Error occurred" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
                         }
 
                     }
