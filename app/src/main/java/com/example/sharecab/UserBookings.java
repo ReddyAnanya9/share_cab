3d6b9131a7c166f0a636ab5dafec42af142a5267
ReddyAnanya9 <58568034+ReddyAnanya9@users.noreply.github.com>
2020-05-02 08:25:10 +0530

    Update UserBookings.java

--- app/src/main/java/com/example/sharecab/UserBookings.java
+++ app/src/main/java/com/example/sharecab/UserBookings.java
@@ -1,25 +1,15 @@
 package com.example.sharecab;
 
-import com.google.firebase.database.Exclude;
 
 public class UserBookings {
     private String Destination,Pickup_pt,Special_Req,time;
     private int Luggage;
-    private String userID;
+  
     public UserBookings(){
 
     }
 
-    @Exclude
-    public String getUserID() {
-        return userID;
-    }
-
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
-
-    public UserBookings(String Destination, String pickup_pt, String special_Req, String time, int luggage) {
+      public UserBookings(String Destination, String pickup_pt, String special_Req, String time, int luggage) {
         this.Destination = Destination;
         Pickup_pt = pickup_pt;
         Special_Req = special_Req;
