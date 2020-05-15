e06719041158b41917e1730ba70771fa37402ac3
ReddyAnanya9 <58568034+ReddyAnanya9@users.noreply.github.com>
2020-05-15 20:32:22 +0530

    Updated ui

--- app/src/main/java/com/example/sharecab/MainActivity.java
+++ app/src/main/java/com/example/sharecab/MainActivity.java
@@ -1,28 +1,35 @@
 package com.example.sharecab;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    Button logoutbtn,book;
+    Button logoutbtn,book,show;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       // mAuth = FirebaseAuth.getInstance();
+        db=FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
+
+       // mAuth = FirebaseAuth.getInstance();
+
         logoutbtn = findViewById(R.id.button);
-        book= findViewById(R.id.button2);
+        book= findViewById(R.id.button6);
+        show=findViewById(R.id.button2);
         logoutbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,8 +44,19 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"Redirecting to form",Toast.LENGTH_SHORT).show();
             }
         });
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),List_bookings.class));
+            }
+        });
+       if(mAuth.getCurrentUser()==null){
+           startActivity(new Intent(getApplicationContext(),Login.class));
+       }
+
     }
-  //  public void signout(View view){
-    //    mAuth.signOut();
-    //}
+
+
+
+
 }
