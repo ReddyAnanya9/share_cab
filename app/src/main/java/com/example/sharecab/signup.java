2c169dab2682eb152f30cadbe029b9dadb0bda94
ReddyAnanya9 <58568034+ReddyAnanya9@users.noreply.github.com>
2020-05-15 20:34:18 +0530

    updated ui

--- app/src/main/java/com/example/sharecab/signup.java
+++ app/src/main/java/com/example/sharecab/signup.java
@@ -1,20 +1,16 @@
 package com.example.sharecab;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-import androidx.multidex.MultiDexApplication;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -22,14 +18,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-
-
-import static android.content.ContentValues.TAG;
 
 public class signup extends AppCompatActivity{
     EditText mFullName,mEmail,mPassword,checkPass,mphoneno;
